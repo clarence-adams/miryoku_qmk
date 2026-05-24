450aa02e9ca2212dd1938af5ee24b5bdc23e4f8a
clarence-adams <clarence.e.a@gmail.com>
2026-05-24 12:12:31 -0400

    Fixed if

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -32,7 +32,6 @@
 // KC_A, KC_S, KC_D,   KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,     KC_QUOT,\
 // KC_Z, KC_X, KC_C,   KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,\
 // U_NP, U_NP, KC_ESC, KC_SPC,  KC_TAB, KC_ENT,  KC_BSPC, LT(U_FUN,KC_DEL),  U_NP, U_NP
-#if defined (KEYBOARD_bastardkb_scylla_v2_splinky_3)
 #define MIRYOKU_ALTERNATIVES_GAME \
 KC_TILD, KC_1, KC_2, KC_3,   KC_4,    KC_5,   KC_6,    KC_7,    KC_8,    KC_9,     KC_0,   \
 U_NP, KC_Q, KC_W, KC_E,   KC_R,    KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,   \
@@ -40,5 +39,5 @@ KC_LSHFT, KC_A, KC_S, KC_D,   KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L, 
 U_NP, KC_Z, KC_X, KC_C,   KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,\
 U_NP, U_NP, KC_ESC, KC_SPC,  KC_TAB, KC_ENT,  KC_BSPC, LT(U_FUN,KC_DEL),  U_NP, U_NP
 
-#if defined (KEYBOARD_bastardkb_scylla_v2_splinky_3)
 #define MIRYOKU_LAYER_EXTRA MIRYOKU_ALTERNATIVES_GAME
+#endif

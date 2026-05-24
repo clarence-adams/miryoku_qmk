c0242d319ac45051f403cda43add5f7a3c4aedeb
clarence-adams <clarenceadams@protonmail.com>
2026-05-24 14:15:19 -0400

    Finally fixed game layer

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -12,20 +12,18 @@ KC_A, KC_S, KC_D,   KC_F,    KC_G,   KC_H,    KC_J,    KC_K,    KC_L,     KC_QUO
 KC_Z, KC_X, KC_C,   KC_V,    KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,\
 U_NP, U_NP, KC_ESC, KC_SPC,  KC_TAB, KC_ENT,  KC_BSPC, LT(U_FUN,KC_DEL),  U_NP, U_NP
 */
-#define MIRYOKU_ALTERNATIVES_GAME( \
+#define MIRYOKU_LAYERMAPPING_EXTRA( \
      K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
     ) \
 LAYOUT_split_4x6_5( \
-U_NP, KC_TILD,  KC_1, KC_2,   KC_3,   KC_4,         KC_5,   KC_6,    KC_7,    KC_8,              KC_9,     KC_0,   \
-U_NP, U_NP,     KC_Q, KC_W,   KC_E,   KC_R,         KC_T,   KC_Y,    KC_U,    KC_I,              KC_O,     KC_P,   \
-U_NP, KC_LSHFT, KC_A, KC_S,   KC_D,   KC_F,         KC_G,   KC_H,    KC_J,    KC_K,              KC_L,     KC_QUOT,\
-U_NP, U_NP,     KC_Z, KC_X,   KC_C,   KC_V,         KC_B,   KC_N,    KC_M,    KC_COMM,           KC_DOT,   KC_SLSH,\
-                      KC_ESC, KC_SPC, KC_TAB,       KC_ENT,  KC_BSPC, LT(U_FUN,KC_DEL)\
-U_NP,   U_NP,         U_NP,     U_NP \
+U_NP, KC_TILD,  KC_1, KC_2,   KC_3,   KC_4,         KC_5,   KC_6,    KC_7,    KC_8,    KC_9,     KC_0,   \
+U_NP, U_NP,     KC_Q, KC_W,   KC_E,   KC_R,         KC_T,   KC_Y,    KC_U,    KC_I,    KC_O,     KC_P,   \
+U_NP, KC_LSFT, KC_A, KC_S,   KC_D,   KC_F,         KC_G,   KC_H,    KC_J,    KC_K,    KC_L,     KC_QUOT,\
+U_NP, U_NP,     KC_Z, KC_X,   KC_C,   KC_V,         KC_B,   KC_N,    KC_M,    KC_COMM, KC_DOT,   KC_SLSH,\
+                      KC_ESC, KC_SPC, KC_TAB,       KC_ENT,  KC_BSPC, LT(U_FUN,KC_DEL),\
+                              U_NP,   U_NP,         U_NP,     U_NP \
 )
-
-#define MIRYOKU_LAYER_EXTRA MIRYOKU_ALTERNATIVES_GAME
 #endif

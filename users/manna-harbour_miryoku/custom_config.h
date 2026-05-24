3ae6d8250323863d027c58eb06a3713aa404bdd7
clarence-adams <clarenceadams@protonmail.com>
2026-05-23 22:13:54 -0400

    Updated game layer -CA

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -6,9 +6,9 @@
 #pragma once
 
 #define MIRYOKU_ALTERNATIVES_GAME \
-KC_Q,              KC_W,              KC_E,              KC_R,              KC_T,              KC_Y,              KC_U,              KC_I,              KC_O,              KC_P,              \
-KC_A,              KC_S,              KC_D,              KC_F,      KC_G,              KC_H,              LSFT_T(KC_J),      LCTL_T(KC_K),      LALT_T(KC_L),      LGUI_T(KC_QUOT),   \
-LT(U_BUTTON,KC_Z), ALGR_T(KC_X),      KC_C,              KC_V,              KC_B,              KC_N,              KC_M,              KC_COMM,           KC_DOT,    KC_SLSH,\
-U_NP,              U_NP,              KC_ESC,KC_SPC,  KC_TAB,KC_ENT,  KC_BSPC, LT(U_FUN,KC_DEL),  U_NP,              U_NP
+KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U,    KC_Y,   KC_QUOT,\
+KC_A, KC_R, KC_S, KC_T, KC_G, KC_M, KC_N, KC_E,    KC_I,   KC_O,   \
+KC_Z, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMM, KC_DOT, KC_SLSH,\
+U_NP, U_NP, KC_ESC,KC_SPC,  KC_TAB,KC_ENT, KC_BSPC, LT(U_FUN,KC_DEL), U_NP, U_NP
 
 #define MIRYOKU_LAYER_EXTRA MIRYOKU_ALTERNATIVES_GAME
